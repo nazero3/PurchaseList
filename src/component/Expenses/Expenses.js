8565a0ec78e6c6cc3029e8443a9c2486d3df20e2
nazero3 <nazerrabah1994@gmail.com>
2022-07-30 16:19:24 +0300

    j

--- src/component/Expenses/Expenses.js
+++ src/component/Expenses/Expenses.js
@@ -1,31 +1,36 @@
-import React from "react";
+import React ,{useState} from "react";
 import "./Expenses.css";
 import ExpenseItem from "./ExpenseItem";
 import Card from "../UI/Card"
+import ExpensesFilter from "./ExpensesFilter";
 function Expenses(props) {
+  
   return (
-    <Card className="expenses">
-      <ExpenseItem
-        title={props.item[0].title}
-        amount={props.item[0].amount}
-        date={props.item[0].date}
-      />
-      <ExpenseItem
-        title={props.item[1].title}
-        amount={props.item[1].amount}
-        date={props.item[1].date}
-      />
-      <ExpenseItem
-        title={props.item[2].title}
-        amount={props.item[2].amount}
-        date={props.item[2].date}
-      />
-      <ExpenseItem
-        title={props.item[3].title}
-        amount={props.item[3].amount}
-        date={props.item[3].date}
-      />
-    </Card>
+    <div>
+      <ExpensesFilter onSaveExpensesFilter/>
+      <Card className="expenses">
+        <ExpenseItem
+          title={props.item[0].title}
+          amount={props.item[0].amount}
+          date={props.item[0].date}
+        />
+        <ExpenseItem
+          title={props.item[1].title}
+          amount={props.item[1].amount}
+          date={props.item[1].date}
+        />
+        <ExpenseItem
+          title={props.item[2].title}
+          amount={props.item[2].amount}
+          date={props.item[2].date}
+        />
+        <ExpenseItem
+          title={props.item[3].title}
+          amount={props.item[3].amount}
+          date={props.item[3].date}
+        />
+      </Card>
+    </div>
   );
 }
 
