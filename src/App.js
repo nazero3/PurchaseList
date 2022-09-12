060ddc08773af15c49f1d2e2b2f01fa1297e6f4b
nazero3 <nazerrabah1994@gmail.com>
2022-09-12 09:36:59 +0300

    5 sec 2 vid

--- src/App.js
+++ src/App.js
@@ -1,32 +1,33 @@
 
-import React from "react";
+import React, {useState} from "react";
 import Expenses from "./component/Expenses/Expenses";
 import NewExpense from "./component/NewExpenses/NewExpense";
+const initial_expenses = [
+  {
+    id: "e1",
+    title: "Toilet Paper",
+    amount: 94.12,
+    date: new Date(2020, 7, 14),
+  },
+  { id: "e2", title: "New TV", amount: 799.49, date: new Date(2021, 2, 12) },
+  {
+    id: "e3",
+    title: "Car Insurance",
+    amount: 294.67,
+    date: new Date(2021, 2, 28),
+  },
+  {
+    id: "e4",
+    title: "New Desk (Wooden)",
+    amount: 450,
+    date: new Date(2021, 5, 12),
+  }];
 function App() {
-  const expenses = [
-    {
-      id: "e1",
-      title: "Toilet Paper",
-      amount: 94.12,
-      date: new Date(2020, 7, 14),
-    },
-    { id: "e2", title: "New TV", amount: 799.49, date: new Date(2021, 2, 12) },
-    {
-      id: "e3",
-      title: "Car Insurance",
-      amount: 294.67,
-      date: new Date(2021, 2, 28),
-    },
-    {
-      id: "e4",
-      title: "New Desk (Wooden)",
-      amount: 450,
-      date: new Date(2021, 5, 12),
-    },
-  ];
+  const [expenses, setExpenses] = useState(initial_expenses);
+  
+  
   const onSaveNewExpenseHandler = (enteredValue) => {
-    console.log('in app js');
-    console.log(enteredValue);
+    setExpenses();
   }
   return (
     <div>
