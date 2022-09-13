1a3304ceb81278a9a8973aea0a68541d2609b666
nazero3 <nazerrabah1994@gmail.com>
2022-09-13 18:17:00 +0300

    lec 67

--- src/component/Expenses/Expenses.js
+++ src/component/Expenses/Expenses.js
@@ -10,25 +10,29 @@ function Expenses(props) {
     setFilteredYear(selectedYear);
   };
 
-    const filteredExpenses = props.items.filter(expense => {
-      return expense.date.getFullYear().toString() === filteredYear;
-    });
-  
+  const filteredExpenses = props.items.filter((expense) => {
+    return expense.date.getFullYear().toString() === filteredYear;
+  });
+
+  let expenseContent = <p>No items found!</p>;
+  if (filteredExpenses.length > 0){
+    expenseContent = filteredExpenses.map((expense) => (
+      <ExpenseItem
+        key={expense.id}
+        title={expense.title}
+        amount={expense.amount}
+        date={expense.date}
+      />
+    ))
+  }
+
   return (
     <Card className="expenses">
       <ExpensesFilter
         selected={filteredYear}
         onChangeFilter={filterChangeHandler}
       />
-      {filteredExpenses.map((expense) => (
-        <ExpenseItem
-          key={expense.id}
-          title={expense.title}
-          amount={expense.amount}
-          date={expense.date}
-        />
-      ))}
-
+      {expenseContent}
       
     </Card>
   );
